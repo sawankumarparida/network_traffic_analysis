a02296435c69bf28608a36c624c3693141d093c5
sawankumarparida <sawankumarparida@gmail.com>
2026-04-30 08:04:18 +0000

    dataset

---
+++ main.R
@@ -0,0 +1,24 @@
+set.seed(123)
+num_records <- 1000
+
+timestamps <- seq.POSIXt(from = as.POSIXct("2024-06-03 00:00:00"), 
+                         by = "hour", length.out = num_records)
+
+generate_ipv4 <- function(n) {
+  paste(sample(0:255, n, replace = TRUE), sample(0:255, n, replace = TRUE), 
+        sample(0:255, n, replace = TRUE), sample(0:255, n, replace = TRUE), sep = ".")
+}
+
+source_ips <- generate_ipv4(num_records)
+destination_ips <- generate_ipv4(num_records)
+
+bytes_transferred <- sample(100:10000, num_records, replace = TRUE)
+
+traffic_data <- data.frame(
+  timestamp = timestamps,
+  source_ip = source_ips,
+  destination_ip = destination_ips,
+  bytes_transferred = bytes_transferred
+)
+
+head(traffic_data)
